4af51d31ed6a78e66a3fe1e80e4ef84a58fc7d75
ArjobanSingh <arjobansingh1998@gmail.com>
2021-10-09 11:24:15 -0700

    added class-validator

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,31 @@
 import "reflect-metadata";
+import "./config";
+
 import { createConnection } from "typeorm";
 import express from "express";
+import cors from 'cors';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 createConnection()
   .then(async (connection) => {
+    app.use(cors(corsOptions));
     app.use(express.json());
+
+    app.use((error, req, res, next) => {
+      console.log("error", error, error.message, error.name);
+      res.status(500).json({
+        error,
+      });
+    });
+  
     app.listen(PORT, () => `Listening on port ${PORT}`);
   })
   .catch((error) => console.log(error));
